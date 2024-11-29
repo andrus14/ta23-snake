ae54965486cca2e0f9cc27cae1eb92795347e2d5
Andrus <andrus.naulainen@ametikool.ee>
2024-11-29 14:12:16 +0200

    high score

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const gameBoardTable = document.getElementById('game-board');
+const scoreSpan = document.getElementById('score');
+const messageDiv = document.getElementById('message');
+const highScoreSpan = document.getElementById('high-score');
 
 const height = 30;
 const width = 30;
@@ -7,8 +10,17 @@ const food = ['🍉', '🍒', '🥩', '🫘', '🧀', '🥪', '🍕', '🥕'];
 
 let foodY, foodX, foodIndex;
 let direction = 'n';
-const speed = 200;
+const speed = 150;
 let snake = initSnake();
+
+let score = 0;
+scoreSpan.innerText = score;
+
+let highScore = localStorage.getItem('high_score');
+if ( !highScore ) {
+    highScore = 0;
+}
+highScoreSpan.innerText = highScore;
 
 document.addEventListener('keydown', e => {
     switch (e.key) {
@@ -30,7 +42,6 @@ document.addEventListener('keydown', e => {
 const intervalId = setInterval(runGame, speed);
 
 generateFood();
-// drawGameBoard();
 
 function runGame () {
     updateSnake();
@@ -38,9 +49,14 @@ function runGame () {
 }
 
 function generateFood () {
-    foodY = Math.floor(Math.random() * height);
-    foodX = Math.floor(Math.random() * height);
+    
+    do {
+        foodY = Math.floor(Math.random() * height);
+        foodX = Math.floor(Math.random() * height);
+    } while ( snake.includes(foodY + '_' + foodX) );
+
     foodIndex = Math.floor(Math.random() * food.length);
+
 }
 
 function initSnake () {
@@ -52,30 +68,79 @@ function initSnake () {
 
 function updateSnake () {
     const head = snake[0].split('_');
-    let newY = parseInt(head[0]);
-    let newX = parseInt(head[1]);
+    let headY = parseInt(head[0]);
+    let headX = parseInt(head[1]);
 
     switch ( direction ) {
+        
         case 'n':
-            newY--;
+
+            if ( headY == 0 ) {
+                headY = height - 1;
+            } else {
+                headY--;
+            }
+
             break;
+
         case 's':
-            newY++;
+            
+            if ( headY == height - 1 ) {
+                headY = 0;
+            } else {
+                headY++;
+            }
+
             break;
+
         case 'w':
-            newX--;
+
+            if ( headX == 0 ) {
+                headX = width - 1;
+            } else {
+                headX--;
+            }
+            
             break;
+
         case 'e':
-            newX++;
+
+            if ( headX == width - 1 ) {
+                headX = 0;
+            } else {
+                headX++;
+            }
+
             break;
+
     }
 
-    if ( newY == foodY && newX == foodX ) {
+    if ( snake.includes(headY + '_' + headX) ) {
+        messageDiv.innerText = 'Game over!';
+        messageDiv.classList.remove('hidden');
+        clearInterval(intervalId);
+
+        if ( score > highScore ) {
+            localStorage.setItem('high_score', score);
+        }
+
+    }
+
+    snake.unshift(headY + '_' + headX);
+
+    if ( headY == foodY && headX == foodX ) {
+
         generateFood();
+
+        score++;
+        scoreSpan.innerText = score;
+
+    } else {
+
+        snake.pop();
+
     }
 
-    snake.unshift(newY + '_' + newX);
-    snake.pop();
 }
 
 function drawGameBoard () {
